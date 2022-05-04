f20183314e64fb35a9ed127c627015f74df4399a
kazuki3 <45926663+kazuki3@users.noreply.github.com>
2022-05-05 00:29:01 +0900

    カスタムフックの実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import "./styles.css";
+import { UserCard } from "./components/UserCard";
+import { useAllUsers } from "./hooks/useAllUsers";
 
 export default function App() {
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+
+  const onClickFetchUsers = () => getUsers();
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickFetchUsers}>データ取得</button>
+      <br />
+      {error ? (
+        <p>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
     </div>
   );
 }
